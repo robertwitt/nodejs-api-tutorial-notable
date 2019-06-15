f27bc22c4fa96146dcdc15c838d7f822c51bfc27
robertwitt <robert_witt@icloud.com>
2019-06-15 11:44:16 +0200

    Add connection to MongoDB and post notes.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,16 @@
 module.exports = function(app, db) {
     app.post('/notes', (req, res) => {
-        // You'll create your note here.    
-        res.send('Hello');
+        // You'll create your note here. 
+        const note = {
+            text: req.body.body,
+            title: req.body.title
+        };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({ 'error': 'An error has occured' });
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
     });
 };
---
+++ config/db-template.js
@@ -0,0 +1,1 @@
+module.exports = { url: 'YOUR URL HERE' };
